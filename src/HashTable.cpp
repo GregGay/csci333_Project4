38a66020d3cf1a5dab63130bf8c4513403317a94
greggay <greggay@debian.myhome.westell.com>
2012-12-03 19:12:56 -0500

    All methods seem to be working in my Hash Table

---
+++ src/HashTable.cpp
@@ -0,0 +1,70 @@
+#include "HashTable.h"
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+template <typename V>
+HashTable<V>::HashTable(V def) {
+    table = new list<Entry<V>* >[1009];
+    Def = def;
+}
+/*
+template <template V>
+HashTable<V>::~HashTable() {
+    delete[] table;
+}
+*/
+template <typename V>
+int HashTable<V>::hashFunction(string key) {
+    int size = 1009; 	//prime number
+    int sum = 0;
+    
+    for (int i = 0; i < (int)key.size(); ++i) {
+	  cout << key << endl;
+	  cout << (int) key[i] << endl;
+	  sum += (int)key[i];
+    }
+    
+    int hash = sum%size;
+
+    cout << "hash: " << hash << endl;
+    return 0;
+}
+
+template <typename V>
+void HashTable<V>::insert(string key, V v) {
+    Entry<V>* temp = new Entry<V>(key, v);
+    int x = hashFunction(key);
+    
+    if (table[x].front()->getKey() == key) {
+	  table[x].front()->setValue(v);
+    }
+    
+    table[x].push_back(temp);
+}
+
+template <typename V>
+V HashTable<V>::find(string key) {
+    int x = hashFunction(key);
+    list<Entry<V>* > cur = table[x];
+    
+    while(!cur.empty()) {
+	  if (cur.front()->getKey() == key) {
+		return cur.front()->getValue();
+	  }
+	  cur.pop_front();  //Deletes cur element at front, finds next element
+    }
+    
+    return Def;	        //Return default value(0)
+}
+
+template <typename V>
+void HashTable<V>::remove(string key) {
+    insert(key, Def);
+}
+
+template class HashTable<int>;
+template class HashTable<double>;
+template class HashTable<string>;

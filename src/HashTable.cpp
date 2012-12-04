f7e3dbb3838767ac65c697837610584c3d90fd5a
greggay <greggay@debian.myhome.westell.com>
2012-12-03 23:52:51 -0500

    Hash table methods, insert and find, use an iterator now

--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -1,5 +1,6 @@
 #include "HashTable.h"
 #include <iostream>
+#include <list>
 
 using std::string;
 using std::cout;
@@ -22,8 +23,8 @@ int HashTable<V>::hashFunction(string key) {
     int sum = 0;
     
     for (int i = 0; i < (int)key.size(); ++i) {
-	  cout << key << endl;
-	  cout << (int) key[i] << endl;
+	  cout << "key: " << key << endl;
+	  cout << "ASCII code: " << (int) key[i] << endl;
 	  sum += (int)key[i];
     }
     
@@ -37,11 +38,18 @@ template <typename V>
 void HashTable<V>::insert(string key, V v) {
     Entry<V>* temp = new Entry<V>(key, v);
     int x = hashFunction(key);
+    typename list<Entry<V>* >::iterator it;
     
-    if (table[x].front()->getKey() == key) {
-	  table[x].front()->setValue(v);
+    for (it = table[x].begin(); it != table[x].end(); ++it) {
+	  if ((*it)->getKey() == key) {
+		(*it)->setValue(v);
+		return;
+	  }
+	  //if (table[x].front()->getKey() == key) {
+	//	table[x].front()->setValue(v);
+	  //}
     }
-    
+
     table[x].push_back(temp);
 }
 
@@ -49,13 +57,19 @@ template <typename V>
 V HashTable<V>::find(string key) {
     int x = hashFunction(key);
     list<Entry<V>* > cur = table[x];
-    
-    while(!cur.empty()) {
+
+    typename list<Entry<V>* >::iterator it;
+    for (it = table[x].begin(); it != table[x].end(); ++it) {
+	  if ((*it)->getKey() == key) {
+		return (*it)->getValue();
+	  }
+    }
+    /*while(!cur.empty()) {
 	  if (cur.front()->getKey() == key) {
 		return cur.front()->getValue();
 	  }
 	  cur.pop_front();  //Deletes cur element at front, finds next element
-    }
+    }*/
     
     return Def;	        //Return default value(0)
 }
